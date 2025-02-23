e417c83b90b34030a9441af8d0d9adce07f63df8
Benji <143539382+Benj011@users.noreply.github.com>
2025-02-23 14:47:07 -0500

    Create lidarTest.py
    Sending commands to tss with an option to unpack lidar data. Still not receiving lidar requests.

---
+++ LunarLink/lidarTest.py
@@ -0,0 +1,40 @@
+import requests
+import socket
+import time
+import struct
+
+URL = "data.cs.purdue.edu"
+PORT = 14141
+
+def parse_tss_response(data, cmd_num):
+    id1 = int.from_bytes(data[:4])
+    id2 = int.from_bytes(data[4:8])
+    floats = 0;
+    # Unpack 13 floats from the remaining data (13 * 4 bytes)
+    if (cmd_num == 167): # this is to check for lidar command 
+        print("HERE")
+        floats = list(struct.unpack('>13f', data[8:8+4*13]))
+    else:
+        floats = struct.unpack('>f', data[8:])
+
+    return id1, id2, floats
+
+
+def get_tss_data(clientSocket, 
+				addr = (URL, PORT), 
+				cmd_num = 58, 
+				input_data = 1, 
+				tstamp = 'now'):
+	# by default gets the EVA time for team 1
+	if tstamp == 'now':
+		tstamp = int(time.time())
+
+	clientSocket.sendto(tstamp.to_bytes(4) + cmd_num.to_bytes(4) + struct.pack('>f', input_data), addr)
+	data, server = clientSocket.recvfrom(1024)
+	print("Received", len(data), "bytes")  # This line prints the number of bytes
+	return parse_tss_response(data, cmd_num=cmd_num)
+
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+timeStamp, command, floats = get_tss_data(clientSocket, cmd_num=167)
+print(floats)
