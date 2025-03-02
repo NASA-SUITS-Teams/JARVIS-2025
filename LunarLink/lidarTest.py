d50f3d55971543cde0b99ce2d8004a5827fc0cb8
Benj011 <bemini@purdue.edu>
2025-03-02 13:17:19 -0500

    added error checking and fixed a print statment

--- LunarLink/lidarTest.py
+++ LunarLink/lidarTest.py
@@ -7,17 +7,19 @@ URL = "data.cs.purdue.edu"
 PORT = 14141
 
 def parse_tss_response(data, cmd_num):
-    id1 = int.from_bytes(data[:4])
-    id2 = int.from_bytes(data[4:8])
-    floats = 0
-    # Unpack 13 floats from the remaining data (13 * 4 bytes)
-    if (cmd_num == 167): # this is to check for lidar command 
-        print("HERE")
-        floats = list(struct.unpack('>13f', data[8:8+4*13]))
-    else:
-        floats = struct.unpack('>f', data[8:])
+	time = int.from_bytes(data[:4])
+	commandNum = int.from_bytes(data[4:8])
+	floats = 0
+	# Unpack 13 floats from the remaining data (13 * 4 bytes)
+	if (cmd_num == 167): # this is to check for lidar command 
+		try:
+			floats = list(struct.unpack('>13f', data[8:8+4*13]))
+		except:
+			print("Did not receive enough bytes")        
+	else:
+		floats = struct.unpack('>f', data[8:])
 
-    return id1, id2, floats
+	return time, commandNum, floats
 
 
 def get_tss_data(clientSocket, 
@@ -36,5 +38,6 @@ def get_tss_data(clientSocket,
 
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-timeStamp, command, floats = get_tss_data(clientSocket, cmd_num=167)
+
+timeStamp, command, floats = get_tss_data(clientSocket, cmd_num=167) # This is the command to send 
 print(floats)
