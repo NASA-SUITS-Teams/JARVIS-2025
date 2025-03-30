6d34e6851b02fec443f6a34a6900cd0ba9e6465a
Conor Ebeling <conorebeling@gmail.com>
2025-03-30 13:25:33 -0400

    Clean up LunarLink folder

--- LunarLink/testSend.py
+++ LunarLink/testSend.py
@@ -5,15 +5,16 @@ import time
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 MESSAGE = b"Hi Benji!"
+
 print(f"UDP target IP: {UDP_IP}")
 print(f"UDP target port: {UDP_PORT}")
 print(f"message: {MESSAGE}")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # INTERNET, sUDP
 
 timeStamp = int(time.time()) # time stamp for command same as tss
 cmdNum = 165 # command number same as tss as well
 inputData = 1 # input data same as tss
-
 
 data = (
     timeStamp.to_bytes(4, byteorder='big') +
