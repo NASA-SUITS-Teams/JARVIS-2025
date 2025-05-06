be3a334794dd618a6c785d8a5c40592486fa4565
PeterZaka <PeterZaka70@gmail.com>
2025-05-05 23:01:59 -0400

    Merge llm tests into one with command line arguments

--- LLM/tests/llm_full_test.py
+++
@@ -1,7 +0,0 @@
-from LLM.utils.ChatBot import CHAT_MODEL, ChatBot
-
-chatbot = ChatBot(model=CHAT_MODEL, use_rag=True, use_tools=True)
-
-while True:
-    user = input("User: ")
-    response = chatbot.get_response_stream(user, just_print=True)
--- LLM/tests/llm_test.py
+++ LLM/tests/llm_test.py
@@ -1,6 +1,12 @@
+import argparse
 from LLM.utils.ChatBot import CHAT_MODEL, ChatBot
 
-chatbot = ChatBot(model=CHAT_MODEL)
+parser = argparse.ArgumentParser()
+parser.add_argument('--use_rag', action='store_true')
+parser.add_argument('--use_tools', action='store_true')
+args = parser.parse_args()
+
+chatbot = ChatBot(model=CHAT_MODEL, use_rag=args.use_rag, use_tools=args.use_tools)
 
 while True:
     user = input("User: ")
