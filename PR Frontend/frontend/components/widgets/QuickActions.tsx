9c95a1cf8a4c1d73d4aef30afa02b3c4a9e3efcf
Conor Ebeling <conorebeling@gmail.com>
2025-05-17 00:38:30 -0400

    Remove calc best path from quick actions

--- PR Frontend/frontend/components/widgets/QuickActions.tsx
+++ PR Frontend/frontend/components/widgets/QuickActions.tsx
@@ -1,5 +1,5 @@
 import { useAPI } from "@/hooks/useAPI";
-import { AlertCircle, MapIcon, Satellite, ScanSearchIcon } from "lucide-react";
+import { AlertCircle, MapIcon, ScanSearchIcon } from "lucide-react";
 
 export default function QuickActions({ setAddPinClicked, addPinClicked }) {
   const { resetPins } = useAPI();
@@ -8,10 +8,6 @@ export default function QuickActions({ setAddPinClicked, addPinClicked }) {
     <div className="p-3">
       <div className="text-sm font-bold text-blue-300 mb-2">QUICK ACTIONS</div>
       <div className="grid grid-cols-2 gap-2 text-xs">
-        <button className="p-2 bg-blue-900/50 hover:bg-blue-800/50 border border-blue-600 rounded-md flex flex-col items-center">
-          <Satellite size={16} className="mb-1" />
-          <span>Calc Best Path</span>
-        </button>
         <button
           className={`p-2 ${
             addPinClicked
