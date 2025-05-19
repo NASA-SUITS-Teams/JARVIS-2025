39cdce6aeaea7f72bf10e928bc6b06b6155e00cc
paulGreenberg2002 <144622976+paulGreenberg2002@users.noreply.github.com>
2025-05-18 22:46:16 -0400

    Add terrain_scan.py
    includes a callable function terrain_scan( (x,y) ) that creates and downloads a heat map of the local terrain

---
+++ Pathfinding/terrain_scan.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pathfinding
+import os
+
+def terrain_scan(pos):
+    pos = [round(pos[0]), round(pos[1])]
+    indices  = pathfinding.xy_to_index(pos)
+    x = indices[0]
+    y = indices[1]
+
+
+    # load terrain data, note from Conor: this is a crappy way to do this, python kinda sucks, or I suck at python... probably both
+    THIS_DIR    = os.path.dirname(os.path.abspath(__file__))
+    PROJECT_ROOT = os.path.abspath(os.path.join(THIS_DIR, os.pardir))
+    TERRAIN_FILE = os.path.join(PROJECT_ROOT, "Pathfinding", "terrain.txt")
+
+    # later, load like this:
+    terrain = np.loadtxt(TERRAIN_FILE, delimiter=",")
+
+    local_terrain = [ row[ y-10 : y+10 ] for row in terrain[ x-10 : x+10 ] ]
+
+    # swap x and y of local_terrain to switch to x,y coordinates for imshow()
+    temp1 = np.transpose(local_terrain)
+    temp2 = []
+    for i in range(len(temp1)):
+        temp2.append(temp1[-(1 + i)])
+    
+    local_terrain = np.array(temp2)
+
+    # plot heatmap using matplotlib imshow()
+    plt.title("20m x 20m Terrain centered at " + str(x) + "," + str(y))
+    plt.imshow(local_terrain, cmap="inferno")
+    plt.colorbar(label="altitude")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    tick_loc = [0, 10, 19]
+    x_ticks = [str(x-10), str(x), str(x+10)]
+    y_ticks = [str(y+10), str(y), str(y-10)]
+    plt.xticks(ticks=tick_loc, labels=x_ticks)
+    plt.yticks(ticks=tick_loc, labels=y_ticks)
+
+    plt.savefig("terrain_" + str(x) + "_" + str(y))
+
+
